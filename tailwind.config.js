8c9f6969c34fda0a1f31755e68f0ede7e62900c1
syauqy syarof ahmad <syauqy.ahmad057@gmail.com>
2025-06-15 15:27:17 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors : {
+        back : '#5A827E',
+        judul : '#FAFFCA',
+      },
+    },
+  },
+  plugins: [],
+}
